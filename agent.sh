7850d2df9791daffeba3bd77d2d458b77ea29d4d
Akash <kashthus@gmail.com>
2021-06-11 12:04:18 +0530

    Fix service error and wp-cli error

---
+++ agent.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function agentInstall() {
+    mkdir /usr/Hosting/
+    cd /usr/Hosting
+    wget -O agent https://github.com/AKASHRP98/agent/releases/download/main/agent
+    chmod +x agent
+    wget -O config.json https://raw.githubusercontent.com/AKASHRP98/agent/master/config.json
+
+    cat >>/etc/systemd/system/agent.service <<EOL
+[Unit]
+Description=Hosting agent
+
+[Service]
+Type=simple
+ExecStart=/usr/Hosting/agent
+KillMode=process
+User=root
+Group=root
+Restart=on-failure
+SuccessExitStatus=2
+
+[Install]
+WantedBy=multi-user.target
+
+EOL
+
+    systemctl daemon-reload
+
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar wp-cli
+
+}
+
+agentInstall
+service agent start
