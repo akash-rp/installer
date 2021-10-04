ffef29bb3c933d957b08dd371021ca2b091ea43a
Akash <kashthus@gmail.com>
2021-10-04 15:29:53 +0530

    update

--- hosting.sh
+++ hosting.sh
@@ -176,8 +176,9 @@ function misc() {
     mkdir /opt/Hosting/certs
     curl -s https://kopia.io/signing-key | sudo apt-key add -
     echo "deb http://packages.kopia.io/apt/ stable main" | sudo tee /etc/apt/sources.list.d/kopia.list
-    sudo apt update
-    sudo apt install kopia
+
+    sudo apt-get install -y kopia
+    sudo apt-get install -y mydumper
     mkdir -p /var/logs/Hosting/
 }
 
