fb5853203bb784f9f6b8b0ab11549f5464c22660
AKASH KASHU <kashthus@gmail.com>
2022-06-10 11:44:30 +0530

    update mariadb to 10.8

--- hosting.sh
+++ hosting.sh
@@ -15,8 +15,8 @@ function mariadbInstall() {
     apt-get update -y
     apt-get install -y software-properties-common
     apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-    add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.4/ubuntu focal main'
-    add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.4/ubuntu focal main'
+    add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.8/ubuntu focal main'
+    add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.8/ubuntu focal main'
     apt-get update -y
     apt-get install -y mariadb-server
     systemctl start mysql
